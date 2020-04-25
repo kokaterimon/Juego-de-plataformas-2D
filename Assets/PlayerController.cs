3768a5afb153cd59b7693823e030da9f3b4ede4e
kokaterimon <bpaucarm@gmail.com>
2020-04-24 22:51:37 -0500

    En el tutorial 5

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
 
-    public float maxSpeed = 10f;
+    public float maxSpeed = 5f;
     public float speed = 2f;
+    public bool grounded;
+    public float jumpPower = 6.5f;
 
     private Rigidbody2D rb2d;
+    private Animator anim;
+    private bool jump;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-                
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("Grounded", grounded);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate()
@@ -32,9 +43,22 @@ public class PlayerController : MonoBehaviour{
             rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
         }
 
-        if (rb2d.velocity.x < -maxSpeed)
+        float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+
+        if (h > 0.1f)
         {
-            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (h < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }   
+
+        if (jump)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
 
         Debug.Log(rb2d.velocity.x);
